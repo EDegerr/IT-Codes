d26229d838e33a5a6b1b5aaeffa2b96f001d0256
Esad Deger <esadeger@gmail.com>
2025-05-16 16:17:03 +0300

    git commit -m "first commit"

--- src/main/java/tests.java
+++ src/main/java/tests.java
@@ -2,11 +2,26 @@ public class tests {
 
     public static void main(String[] args) {
 
-        String s = "Java is Java"
+        String s = "Java is Java";
 
         System.out.println(s.indexOf("Java"));
         //Prints 0 on the console
 
         System.out.println(s.indexOf("va,2"));
-        // Prints on the console
+        // Prints 10 on the console
+
+        System.out.println(s.indexOf('a'));
+        //prints 2 on the console
+
+        System.out.println(s.indexOf('a', 4));
+        //prints 10 on the console,
+
+        System.out.println(s.toLowerCase().lastIndexOf("j")); //prints 8 on the console
+
+        System.out.println(s.toUpperCase().lastIndexOf("V")); //prints -1 on the console
+
+        System.out.println(s.lastIndexOf('v')); //prints 11 on the console
+
+
+    }
 }
