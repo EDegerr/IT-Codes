69c7dbed6c648af453475193f71c496f14aad702
Esad Deger <esadeger@gmail.com>
2025-05-16 16:28:19 +0300

    Pushed sucsessfully

--- src/main/java/tests.java
+++ src/main/java/tests.java
@@ -16,12 +16,26 @@ public class tests {
         System.out.println(s.indexOf('a', 4));
         //prints 10 on the console,
 
-        System.out.println(s.toLowerCase().lastIndexOf("j")); //prints 8 on the console
+        String s2 = "Java is Java";
 
-        System.out.println(s.toUpperCase().lastIndexOf("V")); //prints -1 on the console
+        System.out.println(s2.toLowerCase().lastIndexOf("j")); //prints 8 on the console
 
-        System.out.println(s.lastIndexOf('v')); //prints 11 on the console
+        System.out.println(s2.toUpperCase().lastIndexOf("V")); //prints -1 on the console
 
+        System.out.println(s2.lastIndexOf('v')); //prints 11 on the console
 
+        System.out.println(s2.lastIndexOf("va")); //prints 10 on the console
+
+        String str1 = "";
+        System.out.println(str1.isEmpty());
+
+        String str2 = "Hello";
+        System.out.println(str2.isEmpty());
+
+        String str3 = " ";
+        System.out.println(str3.isBlank());
+
+        String str4 = null;
+        System.out.println(str4.isBlank());
     }
 }
